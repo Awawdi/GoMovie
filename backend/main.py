8b338dd02defa13c5f8793baaf5205e4732c1c3e
orsan <orsan.awawdi@gmail.com>
2024-12-16 13:39:49 +0200

    hello from pod

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,10 @@
+import os
+
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from imdb import IMDb
+import asyncio
+from cachetools import TTLCache
 
 app = FastAPI()
 
@@ -12,13 +17,35 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-@app.get("/")
-async def read_root():
-    return {"message": "Welcome to the FastAPI application"}
+ia = IMDb()
+cache = TTLCache(maxsize=100, ttl=3600)
+
+@app.get("/pod")
+async def read_pod():
+    return {"Hello": f"from: {os.environ.get('HOSTNAME', 'localhost')}"}
 
 @app.post("/search")
 async def search(request: Request):
     data = await request.json()
     search_query = data.get("query")
+
+    if search_query in cache:
+        return cache[search_query]
+
+    results = await asyncio.to_thread(ia.search_movie, search_query)
+    results = results[:10]
     print(f"Search query: {search_query}")
-    return {"message": f"Received search query: {search_query}"}
+
+    movies = []
+    for result in results:
+        movie = await asyncio.to_thread(ia.get_movie, result.movieID)
+        movies.append({
+            "id": f"tt{result.movieID}",
+            "name": movie.get('title'),
+            "year": movie.get('year'),
+            "parental_guide": movie.get('certificates'),
+            "rating": movie.get('rating')
+        })
+
+    cache[search_query] = movies
+    return movies
