17e3afbd16fda7fc7fef1695d1e79fb944cea922
orsan <orsan.awawdi@gmail.com>
2024-12-24 18:03:56 +0200

    added shared library

---
+++ vars/readCurrentVersion.groovy
@@ -0,0 +1,34 @@
+def call(String versionFile, String versionIncrement) {
+    if (fileExists(versionFile)) {
+        def versionContent = readFile(versionFile).trim()
+        def versionParts = versionContent.tokenize('.')
+
+        if (versionParts.size() == 3) {
+            MAJOR_VERSION = versionParts[0].toInteger()
+            MINOR_VERSION = versionParts[1].toInteger()
+            PATCH_VERSION = versionParts[2].toInteger()
+        } else {
+            error("Invalid version format in ${versionFile}. Expected format: x.y.z")
+        }
+    } else {
+        MAJOR_VERSION = 1
+        MINOR_VERSION = 0
+        PATCH_VERSION = 0
+    }
+
+    if (versionIncrement == 'MAJOR') {
+        MAJOR_VERSION++
+        MINOR_VERSION = 0
+        PATCH_VERSION = 0
+    } else if (versionIncrement == 'MINOR') {
+        MINOR_VERSION++
+        PATCH_VERSION = 0
+    } else if (versionIncrement == 'PATCH') {
+        PATCH_VERSION++
+    } else {
+        error("Invalid VERSION_INCREMENT parameter: ${versionIncrement}")
+    }
+
+    IMAGE_TAG = "${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}"
+    echo "Generated Image Tag: ${IMAGE_TAG}"
+}
